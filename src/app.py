e6e4b7854f9edcb056bf44547aad74b4ba4b7e65
Jhosue Peña <e.andreaperez.a@gmail.com>
2023-05-15 17:13:43 -0400

    Resguardo de la appEncomiendas en Python

---
+++ src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import os
+
+#para acceder al archivo o la carpeta del proyecto
+template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+#unir src y template a la carpeta del proyecto
+template_dir = os.path.join(template_dir, 'src', 'template')
+
+#inicializacion de Flask para que busque el index html y lo inicialize
+app = Flask(__name__, template_folder= template_dir)
+
+
+#Rutas de la aplicación
+@app.route('/') #se coloca '/' para que con el mismo nombre de la aplicacion vayamos a ese index de html
+# esto se hace a traves de una funcion (la llame home porque me hace referencia a lo pincipla)
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
+
+
