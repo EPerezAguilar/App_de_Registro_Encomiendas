8f17f03756b97a593e348daa78839495542f0436
Jhosue Peña <e.andreaperez.a@gmail.com>
2023-05-20 07:58:19 -0400

    nuevo

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 import os
+import database as db #acá importé el archivo de de baso de datos para poder trabajar en él
 
+
+#ESTRUCTURA PARA LANZAR NUESTRA APLICACION A TRAVES DE UN PUERTO CON EL MODULO FLASK
 #para acceder al archivo o la carpeta del proyecto
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 #unir src y template a la carpeta del proyecto
@@ -11,13 +14,15 @@ app = Flask(__name__, template_folder= template_dir)
 
 
 #Rutas de la aplicación
-@app.route('/') #se coloca '/' para que con el mismo nombre de la aplicacion vayamos a ese index de html
-# esto se hace a traves de una funcion (la llame home porque me hace referencia a lo pincipla)
+@app.route('/') #se coloca '/' para que con el mismo nombre de la aplicacion vayamos a ese index de html.
+# esto se hace a traves de una funcion (la llame home porque me hace referencia a lo pincipal)
 def home():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
+#FIN
+
 
 
---
+++ src/database.py
@@ -0,0 +1,9 @@
+import mysql.connector
+database = mysql.connector.connect(
+    host = "localhost", 
+    user = "root",
+    password = "",
+    database = "alojamiento información encomiendas 1"
+)
+#Acá se enlaza la base de datos (MySQL)
+
